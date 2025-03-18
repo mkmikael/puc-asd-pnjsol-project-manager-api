8585a552034e3fc8d624587b55dd966aad3209b6
mikael <mikael@acception.com.br>
2025-03-17 22:12:07 -0300

    Exercicio 3

--- src/domain/use-cases/tasks/create-task.service.ts
+++ src/domain/use-cases/tasks/create-task.service.ts
@@ -9,41 +9,42 @@ import { ProjectsRepositoryService } from 'src/infrastructure/database/repositor
 @Injectable()
 export class CreateTaskService implements BaseUseCase {
 
-    constructor(
-        private readonly usersRepository: UsersRepositoryService,
-        private readonly tasksRepository: TasksRepositoryService,
-        private readonly projcetsRepository: ProjectsRepositoryService,
-    ) {}
+  constructor(
+    private readonly usersRepository: UsersRepositoryService,
+    private readonly tasksRepository: TasksRepositoryService,
+    private readonly projcetsRepository: ProjectsRepositoryService,
+  ) {}
 
-    async execute(payload: {
-        task: CreateTaskDto;
-        userId: number;
-    }): Promise<ITask> {
-        // fetch user data
-        const userData = await this.usersRepository.findById(payload.userId);
-        if (!userData) {
-            throw new Error('Usuário não encontrado');
-        }
-        
-        const projectData = await this.projcetsRepository.findById(
-            payload.task.projectId,
-        );
-
-        if (!projectData) {
-            throw new Error('Projeto não encontrado');
-        }
-        
-        const createdTask = await this.tasksRepository.add({
-            name: payload.task.name,
-            status: payload.task.status,
-            project: projectData,
-            user: { id: userData.id },
-        });
-
-        if (!createdTask) {
-            throw new Error('Erro ao criar tarefa');
-        }
-
-        return createdTask;
+  async execute(payload: {
+    task: CreateTaskDto;
+    userId: number;
+  }): Promise<ITask> {
+    // fetch user data
+    const userData = await this.usersRepository.findById(payload.userId);
+    if (!userData) {
+      throw new Error('Usuário não encontrado');
     }
+
+    const projectData = await this.projcetsRepository.findById(
+      payload.task.projectId,
+      payload.userId,
+    );
+
+    if (!projectData) {
+      throw new Error('Projeto não encontrado');
+    }
+
+    const createdTask = await this.tasksRepository.add({
+      name: payload.task.name,
+      status: payload.task.status,
+      project: projectData,
+      user: { id: userData.id },
+    });
+
+    if (!createdTask) {
+      throw new Error('Erro ao criar tarefa');
+    }
+
+    return createdTask;
+  }
 }
